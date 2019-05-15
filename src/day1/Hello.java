8bd560169bdaaae82b5406bb3097e07c72673557
Wyp997997 <572739103@qq.com>
2019-05-15 12:21:21 +0800

    Initial commit

---
+++ src/day1/Hello.java
@@ -0,0 +1,15 @@
+package day1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Hello {
+    public static void main(String[] args) throws FileNotFoundException {
+        String s = "ssssss";
+        System.out.println(s);
+        FileOutputStream f = new FileOutputStream("sss");
+        FileInputStream f2;
+
+    }
+}
